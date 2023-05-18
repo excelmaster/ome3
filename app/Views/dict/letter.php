b6a26bcec5fd6669e3f851b7e356498b43be6c6f
excelmaster <betfie1@gmail.com>
2023-05-18 18:56:40 -0500

    fixed image size in letter dictionary view

--- app/Views/dict/letter.php
+++ app/Views/dict/letter.php
@@ -17,7 +17,7 @@ $this->section('content');
                 <div class="col-sm-10 direct-chat-text bg-blue"> ESTAS VIENDO EL GLOSARIO DE LA APLICACIÓN <?php echo strtoupper($letter) ;  ?>  </div>
             </div>
             <div class="col-sm-2">
-                <a href="<?php echo base_url('dict/' . $site); ?>"><img src="<?php echo base_url('public/img/'. $site . '/dict/ir_dict.gif') ?>"></a>
+                <a href="<?php echo base_url('dict/' . $site); ?>"><img src="<?php echo base_url('public/img/'. $site . '/dict/back_dictionary.png') ?>"></a>
             </div>
         </div>
         <!-- <div class="card-tools">
@@ -34,10 +34,10 @@ $this->section('content');
             <?php
             foreach ($dicts as $d) {
             ?>
-                <div class="col-sm-2">
-                    <div class="card text-blue bg-transparent">
+                <div class="col-sm-2 dict-img">
+                    <div class="card text-blue bg-transparent">                        
+                        <img src="<?php echo base_url('public/img/' . $site . '/dict/' . $letter . '/' . $letter . '_' .  $d['termino']) . '.png' ; ?>" alt="" class="img-fluid bg-transparent" style="width: 80px;heigth:80px">                        
                         <h5 class="text-center"> <?php echo strtoupper($d['termino']) ; ?></h5>                        
-                        <img src="<?php echo base_url('public/img/' . $site . '/dict/' . $letter . '/' . $letter . '_' .  $d['termino']) . '.png' ; ?>" alt="" class="img-fluid bg-transparent" style="width: 100%;">                        
                     </div>
                 </div>
             <?php
